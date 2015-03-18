be254fb73abe85c521b183039acf3eaa4d9262fc
Neftaly Hernandez <neftaly.hernandez@gmail.com>
2015-03-19 00:47:23 +1300

    Add Gulp watchers

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 "use strict";
 
-/*eslint no-unused-vars: 0 */
+
 var liveServer = require("live-server"),
 
     browserify = require("browserify"),
@@ -11,58 +11,88 @@ var liveServer = require("live-server"),
 
     gulp = require("gulp"),
     gutil = require("gulp-util"),
-    eslint = require("gulp-eslint"),
+    watch = require("gulp-watch"),
+    concat = require("gulp-concat"),
     sourcemaps = require("gulp-sourcemaps"),
+    //minifyCss = require("gulp-minify-css"),
+    eslint = require("gulp-eslint"),
     uglify = require("gulp-uglify");
-/*eslint: no-unused-vars: 2 */
 
 var bundler;
 
+
 /**
- * Task combos
+ * Main tasks
  */
 
-gulp.task("build", ["lint", "bundle"]);
+gulp.task("build", ["html", "css", "js"]);
 
 gulp.task("default", ["build", "live-server"]);
 
 
 /**
- * Live Server
+ * Run Live Server
  */
 
 gulp.task("live-server", function () {
     liveServer.start({
+        //noBrowser: true,
         port: 3333,
-        root: "./",
-        noBrowser: true
+        root: "./"
     });
 });
 
 
 /**
- * ESlint
+ * Build HTML
  */
 
-gulp.task("lint", lint);
+gulp.task("html", ["build-html", "watch-html"]);
 
-function lint () {
-    return gulp.src([
-        "./src/**/*.js",
-        "./src/**/*.jsx",
-        "./gulpfile.js"
-    ])
-        .pipe(eslint())
-        .pipe(eslint.format());
-        //.pipe(eslint.failOnError());
-}
+gulp.task("build-html", function () {
+    return gulp.src("./src/index.html")
+        .pipe(gulp.dest("./dist"));
+});
+
+gulp.task("watch-html", function () {
+    watch("./src/index.html", function () {
+        gulp.start("build-html");
+    });
+});
 
 
 /**
- * Watchify/Browserify
+ * Build CSS
  */
 
-gulp.task("bundle", bundle);
+gulp.task("css", ["build-css", "watch-css"]);
+
+gulp.task("build-css", function () {
+    return gulp.src("./src/**/*.css")
+        .pipe(sourcemaps.init())
+        // A bug in minifyCss prevents "!important" from working
+        //.pipe(minifyCss({
+        //    keepSpecialComments: 0
+        //}))
+        .pipe(concat("app.min.css"))
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("./dist"));
+});
+
+gulp.task("watch-css", function () {
+    watch("./src/**/*.css", function () {
+        gulp.start("build-css");
+    });
+});
+
+
+/**
+ * Build JS
+ */
+
+gulp.task("js", ["lint-js", "watch-js"]);
+
+gulp.task("watch-js", bundle);
 
 function bundle() {
     return bundler.bundle()
@@ -70,7 +100,7 @@ function bundle() {
         .pipe(source("app.min.js"))
         .pipe(buffer())
         .pipe(sourcemaps.init({ loadMaps: true }))
-        //.pipe(uglify())
+        .pipe(uglify())
         .pipe(sourcemaps.write("./"))
         .pipe(gulp.dest("./dist"));
 }
@@ -85,6 +115,24 @@ bundler = watchify(browserify({
 }, watchify.args))
     .transform("babelify")
     .transform("brfs")
-    .on("update", lint)
+    .on("update", lintJs)
     .on("update", bundle)
     .on("log", gutil.log);
+
+
+/**
+ * Lint JS
+ */
+
+gulp.task("lint-js", lintJs);
+
+function lintJs () {
+    return gulp.src([
+        "./src/**/*.js",
+        "./src/**/*.jsx",
+        "./gulpfile.js"
+    ])
+        .pipe(eslint())
+        .pipe(eslint.format());
+        //.pipe(eslint.failOnError());
+}
